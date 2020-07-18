4738f951f95c21e9ffdd1e813d5ba4208f15b5c3
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-07-18 08:58:08 -0500

    Contenido_Actualizado

--- 10 - Trabajando con números/2 - Métodos númericos/js/app.js
+++ 10 - Trabajando con números/2 - Métodos númericos/js/app.js
@@ -4,11 +4,11 @@
 //*** Métodos númericos
 
 
-var numero = "10";
+var numero = "23.32";
 
 
-console.log('Number: ', typeof numero, typeof Number(numero));
-console.log('parseInt: ',  parseInt(numero));
-console.log('parseFloat: ', Number.parseFloat(numero));
-console.log('isNaN: ', isNaN(numero));
-console.log('isInteger: ', Number.isInteger(numero));
+console.log('Number: ', typeof numero, typeof Number(numero)); //Muestra el tipo de dato
+console.log('parseInt: ',  parseInt(numero)); //Convierte el dato a entero
+console.log('parseFloat: ', Number.parseFloat(numero)); //Convierte el dato a flotante
+console.log('isNaN: ', isNaN(numero)); //Realiza una comprobación para saber si el dato es un numero o no
+console.log('isInteger: ', Number.isInteger(numero)); //Realiza una comprobación para saber si el dato es entero o no
