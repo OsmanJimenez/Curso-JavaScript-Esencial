8187b545bfd2162e584a6c6c41ed0e4c563b92ab
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-07-19 11:11:19 -0500

    Contenido_Actualizado

--- 13 - Búsquedas en arreglos/2 - Iterando arreglos con forEach/js/app.js
+++ 13 - Búsquedas en arreglos/2 - Iterando arreglos con forEach/js/app.js
@@ -7,4 +7,4 @@ var platillos = ["ceviche", "tacos", "pasta"];
 
 // platillos.forEach( platillo => console.log(platillo) )
 
-platillos.forEach( (platillo, index) => console.log(index, platillo) )
+platillos.forEach( (platillo, index) => console.log(index, platillo) ) //Muestra la ubicación y el contenido en el arreglo de cada sentencia almacenada
